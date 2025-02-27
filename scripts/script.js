ee8b0f737295ee89f90972729fe47330a4c89ef8
Moureira <51127233+Moureira@users.noreply.github.com>
2025-02-27 10:33:46 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,34 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('shadow-sm');
     }
 });
+
+// carregar menu
+document.addEventListener('DOMContentLoaded', function() {
+    // Carrega o menu do arquivo menu.html
+    fetch('/assets/menu.html') // Caminho absoluto para o menu centralizado
+        .then(response => response.text())
+        .then(data => {
+            // Insere o conteúdo do menu no placeholder
+            document.getElementById('menu-placeholder').innerHTML = data;
+            // Adiciona o evento de clique ao botão do menu
+            setupMenu();
+        })
+        .catch(error => console.error('Erro ao carregar o menu:', error));
+});
+
+function setupMenu() {
+    const menuButton = document.getElementById('menu-button');
+    const menu = document.getElementById('menu');
+
+    menuButton.addEventListener('click', function() {
+        // Alterna a classe 'active' no menu
+        menu.classList.toggle('active');
+    });
+
+    // Fecha o menu se clicar fora dele
+    document.addEventListener('click', function(event) {
+        if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
+            menu.classList.remove('active');
+        }
+    });
+}
